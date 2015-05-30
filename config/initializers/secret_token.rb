3c693e2861b4fa40e632ee3d702c313aaa2d3c8a
Reyzapermanasaputra <mr.kezotmament@gmail.com>
2015-05-30 21:30:04 +0700

    resumes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ResumeApp::Application.config.secret_key_base = '9032d5c060cd7429f063ab599e9bb251230fc95e8e95593c5ac6a4baf92d10df9adce1d44bdcbe107794518a9bd7f19ffa3622874cfad98d33096827da62a6f5'
